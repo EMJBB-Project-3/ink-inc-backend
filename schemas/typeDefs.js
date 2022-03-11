6f7f701914cb7b0a7dbbba14dfd1b9c7633e831c
monstermiles <miles.emmitt.moss@gmail.com>
2022-03-11 14:41:25 -0600

    made a basic outline of a backend for testing our AS3 storage

---
+++ schemas/typeDefs.js
@@ -0,0 +1,27 @@
+const { gql } = require('apollo-server-express');
+
+//type defs is the setup for a class
+const typeDefs = gql`
+ 
+  type User {
+    username: String,
+    email: String,
+    password: String
+  }
+
+  type Post {
+    username: String,
+    title: String,
+    text: String,
+    artist: String,
+    
+  }
+
+  type Query {
+    allUsers: [User]
+    allPosts: [Post]
+
+  }
+`;
+
+module.exports = typeDefs;
