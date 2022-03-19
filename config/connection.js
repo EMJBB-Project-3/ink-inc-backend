ef22a54cfa457aeb6b83858453c7844764de78dd
monstermiles <miles.emmitt.moss@gmail.com>
2022-03-19 10:47:58 -0500

    changed db name

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // Wrap Mongoose around local connection to MongoDB
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/test_db', 
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/ink-inc-db', 
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
