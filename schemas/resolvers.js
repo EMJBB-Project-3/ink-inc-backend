38a2a4c0664636157cd26cbe31623db62e8b993b
monstermiles <miles.emmitt.moss@gmail.com>
2022-03-19 15:37:23 -0500

    changed user resolver

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -14,12 +14,16 @@ const resolvers = {
     // },
   // By adding context to our query, we can retrieve the logged in user without specifically searching for them
   
-  user: async (parent, args, context) => {
-    if (context.user) {
-      return User.findOne({ _id: context.user._id });
-    }
-    throw new AuthenticationError('You need to be logged in!');
-  },
+  // user: async (parent, args, context) => {
+  //   if (context.user) {
+  //     return User.findOne({ _id: context.user._id });
+  //   }
+  //   throw new AuthenticationError('You need to be logged in!');
+  // },
+
+    user: async (parent, {userId}) => {
+      return User.findOne({_id: userId})
+    },
 
     // user: async(parent, { userId }) => {
     //   return User.findOne({ _id: userId });
